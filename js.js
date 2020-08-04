04448c7c286c58d415dc1c6f2a525eb04cee96a9
Aleksandr <burov19966@mail.ru>
2020-08-04 14:59:54 +0300

    tabs first

---
+++ js.js
@@ -0,0 +1,15 @@
+// let tabsLeft = document.getElementsByClassName("tabs-left") [0],
+//     tabsRight = document.getElementsByClassName("tabs-right") [0],
+//     active = document.getElementsByClassName("active") [0],
+
+
+
+
+// tabsRight.addEventListener ("click", function () {
+//     if(tabsLeft.className == "tabsLeftActive"){
+//     tabsLeft.classList.remove("active")
+//     }
+// })
+
+
+
